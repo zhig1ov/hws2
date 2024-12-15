43a628da60b2847d94b72e6afbe16d756f63dd5f
zhig1ov <zhiglov.artem@yandex.ru>
2024-12-15 21:29:06 +0300

    add HW3

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -9,7 +9,7 @@ export type MessagePropsType = {
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
-    console.log(props)
+
     return (
         <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
