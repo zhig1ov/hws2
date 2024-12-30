650671172a4bb51f3a0a7bda550ebfc49d203894
zhig1ov <zhiglov.artem@yandex.ru>
2024-12-30 13:32:11 +0300

    add HW5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,17 +16,17 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                {/*роутинг будут писать студенты
+                в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+                <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR} />} />
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route  path={PATH.PRE_JUNIOR} element={<PreJunior />} />
+                <Route  path={PATH.JUNIOR} element={<Junior />} />
+                <Route  path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route  path='/*' element={<Error404 />} />
             </Routes>
         </div>
     )
